1b6cb6f576451c3f2e80a4092aa1e1a20a01ac51
Nikkushhaa <topnikushaa@gmail.com>
2024-06-02 00:28:10 +0400

    second commit

---
+++ day01/lesson1.py
@@ -0,0 +1,29 @@
+#name არის ცვლადი 
+# = არის ცვლადისთის მნიშვნელობის მიმნჭებელი სიმბოლო
+ #"nika" არის ცვლადისთვის მნიშნელონა
+
+surname = "gikoshvili"
+
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიეტი
+
+name = "nikusha" #ეს არის str (string) ტიპის ცვლადი
+age = 14 #ეს არის int (integer) მთელი რიცხვი
+height = 171.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (booL) ტიპის ცვლადი 
+
+knows_programming = True #True ან False
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False #ჯავასკრიპტული camelcase
+
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+
+print(name+ " "+ surname)
+print(age)
+print(height)
+print("i love programming")
