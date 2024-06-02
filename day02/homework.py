6d19aa7122583cbb9ea1ca186e7a6114206bbb1d
Nikkushhaa <topnikushaa@gmail.com>
2024-06-02 14:31:08 +0400

    third commit

---
+++ day02/homework.py
@@ -0,0 +1,37 @@
+# 1) ექმენი ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაპრინტეთ. 
+name = "Nikusha"
+last_name = "Gikoshvili"
+Hobby = "Programing"
+Country = "Georgia"
+Age = 14
+
+print(name, last_name, Hobby, Country, Age)
+
+
+
+# 2) შექმენით ცვლადები, გადაეცით მნიშვნელობები და შემდგომ დაამატეთ ერთმანეთზე.
+
+G = "Goal"
+O = "Oriented"
+A = "Academy"
+
+print(G + O + A)
+
+
+
+
+
+
+
+# 3)  გააკეთეთ 10 მაგალითი ყველა მათემათიკურ ოპერატორებზე. +,-,*,//,**
+
+print(10 + 10)
+print(30 - 10)
+print(10 * 3)
+print(50 // 2)
+print(12 ** 2)
+print(5 + 10 - 2)
+print(5 * 3 - 11)
+print(7 ** 3 - 12)
+print(120 + 180 + 520)
+print(12450 - 1230 - 400)
